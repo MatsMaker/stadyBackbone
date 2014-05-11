b90a9d55dea1c4c5124d6811acb8ff15e9d9ea69
matsmaker <matsitskyss@gmail.com>
2014-05-11 18:31:31 +0300

    before direct accsess

--- js/index.js
+++ js/index.js
@@ -1,68 +1,26 @@
-var PhotoOne = Backbone.Model.extend({
-    defaults: {
-        src: 'placeholder.js',
-        title: 'an image placeholder',
-        coordinates: [0, 0]
-    },
-    initialize: function() {
-        this.on('change:src', function() {
-            var src = this.get("src");
-            console.log('Image source updated to ' + src);
-        });
-    },
-    changeSrc: function(source) {
-        this.set({
-            src: source
-        });
-    }
+var Todo = Backbone.Model.extend({
+	defaults:{
+		title: '',
+		completed: false
+	}
 });
 
-var somePhoto = new PhotoOne({
-    src: 'test.js',
-    title: 'testing'
+var myTodo = new Todo({
+	title: 'Ste throungh instatiation.'
+});
+console.log('Todo title: ' + myTodo.get('title'));
+console.log('Completed: ' + myTodo.get('completed'));
+
+myTodo.set("title", "Title attibute set through Model.set()");
+console.log('Todo title: ' + myTodo.get('title'));
+
+console.log('Completed: '+myTodo.get('completed'));
+
+myTodo.set({
+	title: 'Both attributes set though Model.set()',
+	completed: true
 });
 
-somePhoto.changeSrc('magic.js');
+console.log('Todo title: '+myTodo.get('title'));
+console.log('Completed: '+myTodo.get('completed'));
 
-
-
-var PhotoTwo = Backbone.Model.extend({
-    validate: function(attibs) {
-        if (attibs.src === undefined) {
-            return "Remember to set a source for your image!";
-        }
-    },
-    initialize: function() {
-        console.log('this model has been initialized');
-        this.on('error', function() {
-            console.log(error);
-        });
-    }
-});
-
-var myPhoto = new PhotoTwo();
-myPhoto.set({
-    title: "On the beach"
-});
-
-
-var PhotoCollection = Backbone.Collection.extend({
-    model: PhotoOne
-});
-
-
-var a = new PhotoOne({
-    title: 'my vacation'
-}),
-b = new PhotoOne({
-    title: 'my holiday'
-}),
-c = new PhotoOne({
-    title: 'my weekend'
-});
-
-var photoCollection = new PhotoCollection([a, b]);
-photoCollection.add(c);
-
-photoCollection.remove([a, c]);
-photoCollection.remove(b);
